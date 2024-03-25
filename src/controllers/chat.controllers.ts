bb434de0ef1d6e6ce7268453eb267b29a90d88ff
Manish Thulung <manish.thulung58@gmail.com>
2024-03-26 01:05:13 +0545

    refactor: api endpoint change

--- src/controllers/chat.controllers.ts
+++ src/controllers/chat.controllers.ts
@@ -10,8 +10,10 @@ export const getChats = async (
   next: NextFunction
 ) => {
   try {
-    const {userId} = req.params
-    const chats = await Chat.find({ participants: userId }).populate("participants")
+    const { userId } = req.params;
+    const chats = await Chat.find({ participants: userId }).populate(
+      "participants"
+    );
     if (!chats) {
       throw new ErrorHandler(404, "chats not found!");
     }
@@ -21,22 +23,22 @@ export const getChats = async (
   }
 };
 
-export const getChatById = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { id } = req.params;
-    const chat = await Chat.findById({ _id: id });
-    if (!chat) {
-      throw new ErrorHandler(404, "chats not found!");
-    }
-    res.status(200).json({ data: chat });
-  } catch (error) {
-    next(error);
-  }
-};
+// export const getChatById = async (
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     const { chatId } = req.query;
+//     const chat = await Chat.findById({ _id: chatId });
+//     if (!chat) {
+//       throw new ErrorHandler(404, "chats not found!");
+//     }
+//     res.status(200).json({ data: chat });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 export const createChat = async (
   req: any,
@@ -44,7 +46,7 @@ export const createChat = async (
   next: NextFunction
 ) => {
   try {
-    const { userId } = req.params;
+    const { userId } = req.query;
     const { chatId, content } = req.body;
 
     const user = await User.findById({ _id: userId });
@@ -73,12 +75,36 @@ export const createChat = async (
       throw new ErrorHandler(404, "chat not found!");
     }
 
-    const message = await Message.create({
+    await Message.create({
       sender: req.user.id,
       content,
       chat: chat._id,
     });
     res.status(200).json({ message: "ok" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMessages = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { chatId } = req.query;
+
+    if (!chatId) {
+      throw new ErrorHandler(403, "chat cannot be null!");
+    }
+
+    const chat = await Chat.findById({ _id: chatId });
+    if (!chat) {
+      throw new ErrorHandler(404, "chat not found!");
+    }
+
+    const messages = await Message.find({ chat: chatId }).populate("sender");
+    res.status(200).json({ data: messages });
   } catch (error) {
     next(error);
   }
--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -1,11 +1,16 @@
 import express from "express";
-import { createChat, getChatById, getChats } from "../controllers/chat.controllers";
+import {
+  createChat,
+  getChats,
+  getMessages,
+} from "../controllers/chat.controllers";
 import { verifyToken } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
 router.get("/user-chats/:userId", getChats);
-router.get("/:id", getChatById);
-router.post("/create/:userId", verifyToken, createChat);
+// router.get("/", getChatById);
+router.get("/messages", getMessages);
+router.post("/send-message", verifyToken, createChat);
 
 export default router;
