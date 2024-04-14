de02858d76c8d8842157c33b1f9e3897605f51d0
Manish Thulung <manish.thulung58@gmail.com>
2024-04-14 23:54:53 +0545

    chat-exist route added

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -3,14 +3,15 @@ import {
   createChat,
   getChats,
   getMessages,
+  chatExist,
 } from "../controllers/chat.controllers";
 import { verifyToken } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
 router.get("/user-chats/:userId", getChats);
-// router.get("/", getChatById);
 router.get("/messages", getMessages);
 router.post("/send-message", verifyToken, createChat);
+router.get("/chat-exist/:senderId/:receiverId", chatExist);
 
 export default router;
